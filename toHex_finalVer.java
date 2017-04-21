d1a24d8e932ec50e0ca7725d0da7b346248f7f77
qpan0831 <pq890831@gmail.com>
2017-04-21 02:21:36 -0500

    Update toHex_finalVer.java

--- toHex_finalVer.java
+++ toHex_finalVer.java
@@ -10,14 +10,14 @@ public class toHex_finalVer {
 	{
 		char[] table={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 		char[] arr = new char[8];
-		int counter=0;
+		int counter=arr.length;
 		while(num!=0)
 		{
 			int temp= num&15;
-			arr[counter++]=table[temp];
+			arr[--counter]=table[temp];
 			num=num>>>4;
 		}
-		for(int i=0;i<arr.length;i++)
+		for(int i=counter;i<arr.length;i++)
 		{
 			System.out.print(arr[i]);
 		}
