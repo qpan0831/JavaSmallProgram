5a295881c5c8c3c097ea0bf665f1c55c1d15ab71
qpan0831 <pq890831@gmail.com>
2017-04-21 02:23:26 -0500

    Update toHex_finalVer.java

--- toHex_finalVer.java
+++ toHex_finalVer.java
@@ -2,12 +2,17 @@
 public class toHex_finalVer {
 	public static void main(String[] args)
 	{
-		toHex_final(60);
+		toHex_final(0);
 		
 	}
 	
 	public static void toHex_final(int num)
 	{
+		if(num==0)
+		{
+			System.out.print('0');
+			return;
+		}
 		char[] table={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 		char[] arr = new char[8];
 		int counter=arr.length;
